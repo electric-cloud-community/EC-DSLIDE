739f9fcf3d85a3280ea989e444b67e7d1c54657d
Greg Maxey <gmaxey@electric-cloud.com>
2015-10-06 16:09:44 -0400

    Move DSL IDE to last tab position #2

--- project/ec_setup.pl
+++ project/ec_setup.pl
@@ -1,6 +1,6 @@
 if ($promoteAction eq 'promote') {
 
-	$view->add("DSL IDE",{position=>2, url => 'plugins/EC-DSLIDE-1.0/index.html'});
+	$view->add("DSL IDE",{position=>10, url => 'plugins/@PLUGIN_KEY@-@PLUGIN_VERSION@/index.html'});
 	
 } elsif ($promoteAction eq 'demote') {
     $view->remove("DSL IDE");
